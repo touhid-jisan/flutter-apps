8e2771070dbb4e66627d375dbf07017cbbc6b81a
touhid-jisan <touhidulislamjisan@gmail.com>
2020-06-04 01:38:36 +0600

    Quiz app - Stateful widget added

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -2,14 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+@override
+class MyApp extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+class MyAppState extends State<MyApp> {
 
-class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    setState(() {
+      index = index+1;  
+    });
+    print(index);
+  }
+
+  var index = 0;
   Widget build(BuildContext context) {
+    var questions = ['Whats your fav color','Whats your fav food'];
+    
     return MaterialApp(
-      home: Text('Quiz App'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'), 
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[index]),
+            
+            RaisedButton(
+              child: Text('Question 1'), 
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Question 2'), 
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Question 3'), 
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Question 4'), 
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
- 
-
